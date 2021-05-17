b0bdb76e4e6e1a1288cf2714377939282055e027
geostash <58206851+geostash@users.noreply.github.com>
2021-05-17 14:01:50 +0530

    Add files via upload
    conda idle activate

---
+++ azureproc.bat
@@ -0,0 +1,5 @@
+@echo off
+call conda activate Azureproc 
+"C:\ProgramData\Anaconda3-2020.02\envs\Azureproc\pythonw.exe" "C:\ProgramData\Anaconda3-2020.02\envs\Azureproc\Lib\idlelib\idle.pyw"
+
+
---
+++ dataproc.bat
@@ -0,0 +1,5 @@
+@echo off
+call conda activate dataproc 
+C:\ProgramData\Anaconda3-2020.02\envs\dataproc\pythonw.exe "C:\ProgramData\Anaconda3-2020.02\envs\dataproc\Lib\idlelib\idle.pyw"
+
+
